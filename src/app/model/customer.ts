5ed39cee1b953f819ee6715c42e08e3fe8746295
Arnold <antaloczi.arnold@gmail.com>
2021-02-22 16:33:53 +0100

    Added content to base classes

--- src/app/model/customer.ts
+++ src/app/model/customer.ts
@@ -1,2 +1,9 @@
+import { Address } from "./address";
+
 export class Customer {
+  id: number = 0;
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  address: Address = new Address();
 }
