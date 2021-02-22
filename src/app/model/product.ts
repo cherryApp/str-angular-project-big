05f2798b08057600916aaf25e74b5b439cfb5c6f
Arnold <antaloczi.arnold@gmail.com>
2021-02-22 17:45:36 +0100

    Product class image property only optional

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -4,8 +4,8 @@ export class Product {
   type: string = '';
   catID: number = 0;
   description: string = '';
-  image: string = '';
   price: number = 0;
   featured: boolean = false;
   active: boolean = true;
+  image?: string = '';
 }
