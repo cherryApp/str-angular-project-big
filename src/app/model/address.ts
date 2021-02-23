261bb2747d8b55d6f1028ebfd9af83b0264de34b
kkovacsics <kalman.kovacsics@gmail.com>
2021-02-23 19:26:50 +0100

    Customer+Address class

--- src/app/model/address.ts
+++ src/app/model/address.ts
@@ -1,15 +1,7 @@
 export class Address {
-<<<<<<< HEAD
-    zip: string = '';
-    country: string = '';
-    city: string = '';
-    street: string = '';
-    notes: string = '';
-=======
     zip: number=0;
   country: string='';
   city: string='';
   street: string='';
   notes: string='';  
->>>>>>> origin/dev
 }
--- src/app/model/customer.ts
+++ src/app/model/customer.ts
@@ -1,15 +1,5 @@
 import { Address } from './address';
 export class Customer {
-<<<<<<< HEAD
-  id: number = 0;
-  firstName: string = '';
-  lastName: string = '';
-  email: string = '';
-  address: Address = new Address();
-  active: boolean=false;
-
-}
-=======
     id: number=0;
   firstName: string='';
   lastName: string='';
@@ -18,4 +8,3 @@ export class Customer {
   active: boolean=false;
 
 }
->>>>>>> origin/dev
