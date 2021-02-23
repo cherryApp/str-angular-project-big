caa928569d5dbb482d7153bd94f2881d659fd8ff
szakalyzs <szakalyzs@gmail.com>
2021-02-23 20:02:46 +0100

    service

---
+++ src/app/service/order.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Order } from '../models/order';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  jsonUrl: string = 'http://localhost:3000/orders';
+
+  list$: BehaviorSubject<Order[]> = new BehaviorSubject<Order[]>([]);
+
+  constructor(
+   private  http: HttpClient
+  ) { }
+
+  getAll(): void {
+    this.http.get<Order[]>(this.jsonUrl).subscribe(
+      orders => this.list$.next(orders)
+    );
+}
+}
