67ae10cbf853f61dc294c7eece9df05fb4261506
szabodev <szabodev@yahoo.com>
2021-02-24 09:32:46 +0100

    changed listing folders

---
+++ src/app/page/listing/listing-customer/listing-customer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-customer',
+  templateUrl: './listing-customer.component.html',
+  styleUrls: ['./listing-customer.component.scss']
+})
+export class ListingCustomerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
