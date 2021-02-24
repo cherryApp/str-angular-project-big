fffdbc27074545cc3a5f707917bbfc47f94e33db
juli59 <molnarjuli59@mail.com>
2021-02-24 12:58:28 +0100

    listing order

--- src/app/page/listing/listing-order/listing-order.component.ts
+++ src/app/page/listing/listing-order/listing-order.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Order } from 'src/app/model/order';
 import { OrderService } from 'src/app/service/order.service';
+import { ConfigService, ITableCol } from '../../../service/config.service';
 
 @Component({
   selector: 'app-listing-order',
@@ -10,14 +12,31 @@ import { OrderService } from 'src/app/service/order.service';
 })
 export class ListingOrderComponent implements OnInit {
   orderList$: BehaviorSubject<Order[]>= this.orderService.list$;
+  // orderList$: Observable<Order[]>= this.orderService.getAll();
   phrase: string='';
+  cols: ITableCol[]= this.configService.orderTableCols
+
   constructor(
-    private orderService:OrderService
+    private orderService:OrderService,
+    private router:Router,
+    private configService: ConfigService
   ) { }
 
+  filterKey: string = 'name';
+  filterKeys: string[] = Object.keys(new Order());
 
 
   ngOnInit(): void {
+    this.orderService.getAll();
   }
+
+  columnKey: string = '';
+  onColumnSelect(key: string): void {
+    this.columnKey = key;
+  }
+  // onRemove(order: Order): void {
+  //   this.orderService.remove(order.id),
+  //   this.router.navigate([''])
+  //  }
 
 }
