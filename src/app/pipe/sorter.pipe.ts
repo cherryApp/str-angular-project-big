81aeb4f4ff993f7acbf12bd8eef56bff2bc9e3cf
thalkirst <peter.bonyai@gmail.com>
2021-02-24 07:22:45 +0100

    product list ordering works fully

--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -5,22 +5,39 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SorterPipe implements PipeTransform {
 
-  transform(value: any[] | null, key: string): any[] | null {
+  transform(value: any[] | null, key: string, sortOrder: string): any[] | null {
     if (!Array.isArray(value) || key === '') {
       return value;
     }
 
-    return value.sort(
-      function (a: any, b: any): any {
-        if (typeof a[key] === 'number' && typeof b[key] === 'number') {
-          return a[key] - b[key];
-        } else {
-          const strItemA: string = ('' + a[key]).toLowerCase();
-          const strItemB: string = ('' + b[key]).toLowerCase();
-          return strItemA.localeCompare(strItemB);
-        }
-      }
-    )
-  }
+    if (sortOrder === "descending") {
 
+      return value.sort(
+        function (a: any, b: any): any {
+          if (typeof a[key] === 'number' && typeof b[key] === 'number') {
+            return b[key] - a[key];
+          } else {
+            const strItemA: string = ('' + a[key]).toLowerCase();
+            const strItemB: string = ('' + b[key]).toLowerCase();
+            return strItemB.localeCompare(strItemA);
+          }
+        }
+      )
+
+    } else {
+
+      return value.sort(
+        function (a: any, b: any): any {
+          if (typeof a[key] === 'number' && typeof b[key] === 'number') {
+            return a[key] - b[key];
+            
+          } else {
+            const strItemA: string = ('' + a[key]).toLowerCase();
+            const strItemB: string = ('' + b[key]).toLowerCase();
+            return strItemA.localeCompare(strItemB);
+          }
+        }
+      )
+    }
+  }
 }
