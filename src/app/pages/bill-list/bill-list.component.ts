533a1d9e30452b4e1ea94ccaf85fbb5605154901
kss-nn <67142065+kss-nn@users.noreply.github.com>
2021-02-24 14:09:28 +0100

    Bill lista - rendezési funkció ---> részben működik
    Az első oszlopnál jól működik a növekvő-csökkenő sorrendbe rendezés, és megjelenik a kis nyilacska. A második-harmadik-negyedik oszlopnál csak a növekvő sorrendbe rendezés működik.

--- src/app/pages/bill-list/bill-list.component.ts
+++ src/app/pages/bill-list/bill-list.component.ts
@@ -2,6 +2,7 @@ import { KeyValue } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Bill, BillAttributes } from 'app/model/bill';
 import { BillService } from 'app/services/bill.service';
+import { ColumnSortOrder } from 'app/services/product.service';
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
@@ -10,13 +11,15 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./bill-list.component.css']
 })
 export class BillListComponent implements OnInit {
+
   billList$: BehaviorSubject<Bill[]> = this.billService.list$;
 
   phrase: string = '';
   filterKey = 'name';
 
-  attributes = new BillAttributes();
+  sorterKey: string ='';
 
+  attributes = new BillAttributes();
 
   constructor(private billService: BillService) { }
 
@@ -24,8 +27,8 @@ export class BillListComponent implements OnInit {
     this.billService.getAll();
   }
 
-  onDelete(product: Bill): void {
-    this.billService.remove(product);
+  onDelete(bill: Bill): void {
+    this.billService.remove(bill);
   }
 
   onChangePhrase(event: Event): void {
@@ -43,5 +46,40 @@ export class BillListComponent implements OnInit {
   originalOrder = (a: KeyValue<number,string>, b: KeyValue<number,string>): number => {
     return 0;
   }
-}
 
+  onColumnSelect(key: string): void {
+    this.sorterKey = key;
+    let clicked = true;
+    
+    if (this.sortOrder[key] === "none" && clicked) {
+      this.eraseSortDirections();
+      this.sortOrder[key] = "ascending"
+      clicked = false;
+    }
+
+    if (this.sortOrder[key] === "ascending" && clicked) {
+      this.eraseSortDirections();
+      this.sortOrder[key] = "descending"
+      clicked = false;
+    }
+
+    if (this.sortOrder[key] === "descending" && clicked) {
+      this.eraseSortDirections();
+      this.sortOrder[key] = "ascending"
+      clicked = false;
+    }
+
+    this.sortDirection = this.sortOrder[key];
+    console.log(this.sortDirection);
+  }
+  
+  eraseSortDirections(): void {
+    for (let key in this.sortOrder) {
+      this.sortOrder[key] = "none";
+    }
+  }
+  
+  sortDirection = "none";
+  
+  sortOrder = new ColumnSortOrder();
+}
