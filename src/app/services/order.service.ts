085f1e878bf2bff7eb4719c3cc8303dcfa3cdde0
szakalyzs <szakalyzs@gmail.com>
2021-02-25 20:47:07 +0100

    Filter order list by status

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Order } from '../models/order';
 
 @Injectable({
@@ -20,5 +20,35 @@ export class OrderService {
     this.http.get<Order[]>(this.jsonUrl).subscribe(
       orders => this.list$.next(orders)
     );
-}
+  }
+
+  get(id: number): Observable<Order> {
+    id = typeof id === 'string' ? parseInt(id, 10) : id;
+    const order: Order | undefined = this.list$.value.find(item => item.id === id);
+    if (order) {
+      return of(order);
+    }
+    return of(new Order());
+  }
+
+  create(order: Order): void {
+    this.http.post<Order>(this.jsonUrl, order).subscribe(
+      () => this.getAll()
+    );
+  }
+
+  update(order: Order): void {
+    this.http.patch<Order>(`{this.jsonUrl}/{order.id}`, order).subscribe(
+      () => this.getAll()
+    );
+  }
+
+  remove(order: Order): void {
+    this.http.delete<Order>(`${this.jsonUrl}/${order.id}`).subscribe(
+      () => this.getAll()
+    );
+  }
+
+
+
 }
