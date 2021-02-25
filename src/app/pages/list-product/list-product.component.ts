3f5af292f0ffc1b89f46c8cb498f6480f2c3e723
Barta Mátyás <l3.matyi@gmail.com>
2021-02-25 11:18:04 +0100

    Product list component base without sorting

--- src/app/pages/list-product/list-product.component.ts
+++ src/app/pages/list-product/list-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service'
 
 @Component({
   selector: 'app-list-product',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductComponent implements OnInit {
 
-  constructor() { }
+  productList$: Observable<Product[]> = this.productService.productList$;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productService.getAll();
   }
 
 }
