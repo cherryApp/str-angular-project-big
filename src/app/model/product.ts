047f5f6cac75cfb93b527db26d8ef5ae89f5dedd
thalkirst <peter.bonyai@gmail.com>
2021-02-28 08:18:07 +0100

    summary line added to end of page

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -5,6 +5,7 @@ export class Product {
     catID: number = 0;
     description: string = '';
     price: number = 0;
+    stock: number = 0;
     featured: boolean = false;
     active: boolean = false;
 }
@@ -16,6 +17,16 @@ export class ProductAttributes {
     catID: string = 'Category'
     description: string = 'Description'
     price: string = 'Price'
+    stock: string = 'Stock'
     featured: string = 'Featured'
     active: string = 'Active'
 }
+
+export class ProductSummaryData {
+    totalProducts: number = 0;
+    totalItems: number = 0;
+    totalValue: number = 0;
+    totalActive: number = 0;
+    totalFeatured: number = 0;
+    totalinCategories: Array<number> = [0,0,0,0,0];
+  }
