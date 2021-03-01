5751d6188909de43ca881fc5a6687445d18850b6
kss-nn <67142065+kss-nn@users.noreply.github.com>
2021-03-01 18:51:40 +0100

    Bills: class javítás, routing javítás
    A Bill osztályt javítottam alapértelmezett értékek hozzáadásával. A Bill oldalon a szerkesztés gomb esetében rossz volt a routing megadása - ezt is javítottam.

--- src/app/model/bill.ts
+++ src/app/model/bill.ts
@@ -1,13 +1,13 @@
 export class Bill {
-  id: number;
-  orderID: number;
-  amount: number;
-  status:string;
+  id: number = 0;
+  orderID: number = 0;
+  amount: number = 0;
+  status: string = '';
   constructor() { }
 }
 export class BillAttributes{
-  id: string= "ID";
-  orderID: string= "Order ID";
-  amount: string= "Amount";
-  status: string= "Status";
+  id: string = 'ID';
+  orderID: string = 'Order ID';
+  amount: string = 'Amount';
+  status: string = 'Status';
 }
