450b8dcc28c043eb978cbfa8b71e92bbbb1621d7
petern1953 <petern1953@gmail.com>
2021-03-01 15:43:38 +0100

    header and card component

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -8,38 +8,42 @@ import { InfoCard } from '../card/card.component';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  footIcon(): string {
+    return Math.floor(Math.random() * 2) ? 'trending_up' : 'trending_down';
+  }
+
   cards: InfoCard[] = [
     {
       cardClass: 'card-header-warning',
-      icon: 'store',
+      icon: 'people_alt',
       title: '117',
-      content: 'Customers',
-      footer: 'footer text',
-      footIcon: 'text-danger',
+      content: 'Vásárlók',
+      footer: 'Vásárlói statisztika: az aktív vásárlók száma',
+      footIcon: this.footIcon(),
     },
     {
       cardClass: 'card-header-success',
-      icon: 'info_outline',
+      icon: 'cases',
       title: '533',
-      content: 'Products',
-      footer: 'footer text',
-      footIcon: 'text-danger',
+      content: 'Termékek',
+      footer: 'Termékstatisztika: az aktív termékek száma',
+      footIcon: this.footIcon(),
     },
     {
       cardClass: 'card-header-danger',
-      icon: 'content_copy',
+      icon: 'shopping_cart',
       title: '729',
-      content: 'Orders',
-      footer: 'footer text',
-      footIcon: 'text-danger',
+      content: 'Rendelések',
+      footer: 'Rendelésstatisztika: még nem fizetett rendelések száma',
+      footIcon: this.footIcon(),
     },
     {
       cardClass: 'card-header-warning',
-      icon: 'update',
+      icon: 'point_of_sale',
       title: '544',
-      content: 'Cash-desk',
-      footer: 'footer text',
-      footIcon: 'text-danger',
+      content: 'Pénztár',
+      footer: 'Pénzügyi statisztika: még nem fizetett számlák összege',
+      footIcon: this.footIcon(),
     },
   ];
   charts: InfoChart[] = [
