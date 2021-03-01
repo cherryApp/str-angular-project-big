322c10874f1404d1f0964ce820b7b639897d5769
Adamnemeth0315 <adam.nemeth.0315@gmail.com>
2021-03-01 08:48:35 +0100

    configservice átírva magyarra

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -22,25 +22,25 @@ export class ConfigService {
 
   orderTableCols: ITableCol[] = [
     { key: 'id', text: '#', editable: false },
-    { key: 'customerID', text: 'CustomerId', editable: false },
-    { key: 'productID', text: 'ProductId', editable: false },
-    { key: 'amount', text: 'Amount', editable: true },
-    { key: 'status', text: 'Status', editable: true }
+    { key: 'customerID', text: 'Vásárló Id', editable: false },
+    { key: 'productID', text: 'Termék Id', editable: false },
+    { key: 'amount', text: 'Összeg', editable: true },
+    { key: 'status', text: 'Státusz', editable: true }
   ];
 
   customerTableCols: ITableCol[] = [
     { key: 'id', text: '#', editable: false },
-    { key: 'firstName', text: 'First Name', editable: true },
-    { key: 'lastName', text: 'Last Name', editable: true },
+    { key: 'firstName', text: 'Keresztnév', editable: true },
+    { key: 'lastName', text: 'Vezetéknév', editable: true },
     { key: 'email', text: 'Email', editable: true },
-    { key: 'address', text: 'Address', editable: true }
+    { key: 'address', text: 'Cím', editable: true },
   ];
 
   billTableCols: ITableCol[] = [
     { key: 'id', text: '#', editable: false },
-    { key: 'orderId', text: 'OrderId', editable: false },
-    { key: 'amount', text: 'Amount', editable: true },
-    { key: 'status', text: 'Status', editable: true }
+    { key: 'orderId', text: 'RendelésId', editable: false },
+    { key: 'amount', text: 'Összeg', editable: true },
+    { key: 'status', text: 'Státusz', editable: true }
   ];
 
   constructor() { }
