754f20c530961ff8b85949464e7e8be9cd245ca5
kisqqcs <kisqqcs@yahoo.com>
2021-03-02 07:44:20 +0100

    Edit order started.

--- src/app/editors/edit-order/edit-order.component.ts
+++ src/app/editors/edit-order/edit-order.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Order, OrderAttributes } from 'app/model/order';
+import { OrderService } from 'app/services/order.service';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-order',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOrderComponent implements OnInit {
 
-  constructor() { }
+  clicked: boolean = false;
+
+  order$: Observable<Order> = this.activatedRoute.params.pipe(
+    switchMap(params => this.orderService.get(params.id))
+  );
+
+  attributes = new OrderAttributes();
+
+  constructor(    
+    private activatedRoute: ActivatedRoute,
+    private orderService: OrderService,
+    private router: Router,) { }
 
   ngOnInit(): void {
   }
 
+  onUpdate(form: NgForm, order: Order): void {
+    this.clicked=true;
+    this.animateSaveIcon();
+    if (order.id === 0) {
+      this.orderService.create(order);
+    } else {
+      this.orderService.update(order).subscribe(
+        ev => this.router.navigate(['order-list'])
+      );
+    }
+  }
+  animateSaveIcon(): void {
+    let saveIcon = document.getElementById("saveicon");
+    saveIcon.classList.remove("fa-save");
+    saveIcon.classList.add("fa-spinner", "fa-pulse");
+  }
 }
