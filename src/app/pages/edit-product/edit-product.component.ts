00595cc082fdec3850cc4d93a2413cf652ac430f
Barta Mátyás <l3.matyi@gmail.com>
2021-03-03 18:05:03 +0100

    edit product

--- src/app/pages/edit-product/edit-product.component.ts
+++ src/app/pages/edit-product/edit-product.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ITableCol, ConfigService } from 'src/app/services/config.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  product: Product = new Product();
+  updating: boolean = false;
+  cols: ITableCol[] = this.configService.tableColsProductList;
+
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private configService: ConfigService,
+    ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      params =>
+      this.productService.getOneById(params.id).subscribe(
+        product => {
+          this.product = product || new Product();
+        }
+      )
+    );
+  }
+
+  onFormSubmit(form: NgForm): void {
+    this.updating = true;
+    this.productService.update(this.product).subscribe(
+      () => this.router.navigate(['products'])
+    );
   }
 
 }
