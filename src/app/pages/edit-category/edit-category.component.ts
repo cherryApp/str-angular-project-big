e9abaa6c85e6d83f821ab8b3a9bc9b397bb07edd
bobbyfromgeminus <info.geminus@gmail.com>
2021-03-04 20:57:58 +0100

    Navbar, footer finishing

--- src/app/pages/edit-category/edit-category.component.ts
+++ src/app/pages/edit-category/edit-category.component.ts
@@ -4,42 +4,66 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Category } from 'src/app/models/category';
 import { ConfigService, ITableCol } from 'src/app/services/config.service';
 import { CategoryService } from 'src/app/services/category.service';
-
+ 
 @Component({
   selector: 'app-edit-category',
   templateUrl: './edit-category.component.html',
   styleUrls: ['./edit-category.component.scss']
 })
 export class EditCategoryComponent implements OnInit {
-
+ 
+  title: string = '';
+  catId: number = 0;
   category: Category = new Category();
   updating: boolean = false;
   cols: ITableCol[] = this.configService.tableColsCategoryList;
-
+ 
   constructor(
     private categoryService: CategoryService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private configService: ConfigService,
   ) { }
-
+ 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(
-      params =>
-        this.categoryService.getOneById(params.id).subscribe(
-          category => {
-            this.category = category || new Category();
-          }
-        )
-    );
+      params => {
+        if (params.id === 0) {
+          this.title = 'Create New Category';
+          this.category = new Category();
+        }
+        else
+          this.categoryService.getOneById(params.id).subscribe(
+            item => {
+              this.catId = params.id;
+              this.title = 'Edit this Category';
+              this.category = item;
+            })
+      }
+    )
   }
-
-  onFormSubmit(form: NgForm): void {
-    this.updating = true;
-    this.categoryService.update(this.category).subscribe(
-      () => this.router.navigate(['categories'])
-    );
+ 
+  onFormSubmit(form: NgForm, element: Category): void {
+    try {
+      if (element.id == 0) {
+        this.categoryService.create(element).subscribe(
+          () => this.router.navigate(['/categories'])
+          );
+          // toaster üzenet sikeres létrehozásról
+          this.configService.showSuccess('Created successfuly.', 'New Category');
+        }
+        else {
+          this.categoryService.update(element).subscribe(
+            () => this.router.navigate(['/categories'])
+          );
+          // toaster üzenet sikeres módosításról
+          this.configService.showSuccess('Updated successfuly.', `Category #${ element.id}`);
+        }
+      } catch (error) {
+        // toaster üzenet hibáról
+        this.configService.showError('Something went wrong .', `Category editor`);
+      }
+    }
+   
   }
-
-
-}
+  
