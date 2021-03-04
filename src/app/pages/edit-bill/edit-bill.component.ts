7bfcbc85b8de58a1811a4be1d8cfbf6bdbdca8db
bobbyfromgeminus <info.geminus@gmail.com>
2021-03-04 09:42:51 +0100

    edit forms standardization + Toastr (bill, order)

--- src/app/pages/edit-bill/edit-bill.component.ts
+++ src/app/pages/edit-bill/edit-bill.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Bill } from 'src/app/models/bill';
 import { BillService } from 'src/app/services/bill.service';
-import { ConfigService } from 'src/app/services/config.service';
+import { ConfigService, ITableCol } from 'src/app/services/config.service';
 
 @Component({
   selector: 'app-edit-bill',
@@ -12,8 +12,11 @@ import { ConfigService } from 'src/app/services/config.service';
 })
 export class EditBillComponent implements OnInit {
 
+  title: string = '';
+  id: number = 0;
   bill: Bill = new Bill();
   updating: boolean = false;
+  cols: ITableCol[] = this.configService.tableColsBillList;
 
   constructor(
     private billService: BillService,
@@ -23,21 +26,43 @@ export class EditBillComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-      this.activatedRoute.params.subscribe(
-        params =>
+    this.activatedRoute.params.subscribe(
+      params =>{
+        if(params.id == 0){
+          this.title = 'Create New Bill';
+          this.bill = new Bill();
+        }
+        else
           this.billService.getOneById(params.id).subscribe(
-            bill => {
-              this.bill = bill || new Bill();
-            }
-          )
-      );
-    }
-
-  onFormSubmit(form: NgForm): void {
-      this.updating = true;
-      this.billService.update(this.bill).subscribe(
-        () =>{}
-        );
-        this.router.navigate(['bills'])
+            item => {
+              this.id = params.id;
+              this.title = 'Edit this Bill';
+              this.bill = item;
+            })
       }
+    )
+  }
+
+  onFormSubmit(form: NgForm, element: Bill): void {
+    try {
+      if (element.id == 0) {
+        this.billService.create(element).subscribe(
+          () => this.router.navigate(['/bills'])
+        );
+        // toaster üzenet sikeres létrehozásról
+        this.configService.showSuccess('Created successfuly.', 'New Bill');
+      }
+      else {
+        this.billService.update(element).subscribe(
+          () => this.router.navigate(['/bills'])
+        );
+        // toaster üzenet sikeres módosításról
+        this.configService.showSuccess('Updated successfuly.', `Bill #${ element.id}`);
+      }
+    } catch (error) {
+      // toaster üzenet hibáról
+      this.configService.showError('Something went wrong .', `Bill editor`);
+    }
+  }
+
 }
--- src/app/pages/edit-order/edit-order.component.ts
+++ src/app/pages/edit-order/edit-order.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Order } from 'src/app/models/order';
+import { OrderService } from 'src/app/services/order.service';
+import { ConfigService, ITableCol } from 'src/app/services/config.service';
 
 @Component({
   selector: 'app-edit-order',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOrderComponent implements OnInit {
 
-  constructor() { }
+  title: string = '';
+  id: number = 0;
+  order: Order = new Order();
+  updating: boolean = false;
+  cols: ITableCol[] = this.configService.tableColsOrderList;
+
+  constructor(
+    private orderService: OrderService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private configService: ConfigService,
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      params =>{
+        if(params.id == 0){
+          this.title = 'Create New Order';
+          this.order = new Order();
+        }
+        else
+          this.orderService.getOneById(params.id).subscribe(
+            item => {
+              this.id = params.id;
+              this.title = 'Edit this Order';
+              this.order = item;
+            })
+      }
+    )
+  }
+
+  onFormSubmit(form: NgForm, element: Order): void {
+    try {
+      if (element.id == 0) {
+        this.orderService.create(element).subscribe(
+          () => this.router.navigate(['/orders'])
+        );
+        // toaster üzenet sikeres létrehozásról
+        this.configService.showSuccess('Created successfuly.', 'New order');
+      }
+      else {
+        this.orderService.update(element).subscribe(
+          () => this.router.navigate(['/orders'])
+        );
+        // toaster üzenet sikeres módosításról
+        this.configService.showSuccess('Updated successfuly.', `Order #${ element.id}`);
+      }
+    } catch (error) {
+      // toaster üzenet hibáról
+      this.configService.showError('Something went wrong .', `Order editor`);
+    }
   }
 
 }
