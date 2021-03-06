ac944d8d8433fa22a92c7883f42dc217a12d8dc6
juli59 <molnarjuli59@mail.com>
2021-03-06 19:33:03 +0100

    m

--- src/app/service/animate-scroll-service.service.ts
+++ src/app/service/animate-scroll-service.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AnimateScrollServiceService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AnimateScrollServiceService {
 
-  constructor() { }
-}
+//   constructor() { }
+// }
